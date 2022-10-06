7cd466dd6b22f828c09ed3e8d211d0351b9506a1
Vladyslavzar <zarubinvlad12@gmail.com>
2022-10-07 02:52:15 +0300

    added from 576px to 992px res support

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(function(){
 
-});
+window.addEventListener('scroll', headerResize);
+
+
+function headerResize() {
+  if (document.documentElement.scrollTop !== 0) {
+    document.querySelector('.header').classList.add("-small");
+  } else {
+    document.querySelector('.header').classList.remove("-small");
+  }
+}
+
+const burgerBtn = document.querySelector('.headerContainerBurgerMenu');
+
+burgerBtn.addEventListener('click', () => {
+	document.querySelector('.header').classList.toggle("-active")
+	document.querySelector('.header__content-left-side').classList.toggle("-active")
+	burgerBtn.classList.toggle("-active")
+})
