57f8ad16555f0537eb6a86867bae8748bd994cfb
Vladyslavzar <zarubinvlad12@gmail.com>
2022-10-08 02:01:57 +0300

    added 1200 res support and above

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 
 window.addEventListener('scroll', headerResize);
 
+const scrollBtn = document.querySelector('.scroll-top-btn');
 
 function headerResize() {
   if (document.documentElement.scrollTop !== 0) {
     document.querySelector('.header').classList.add("-small");
+    scrollBtn.classList.add("-show");
   } else {
     document.querySelector('.header').classList.remove("-small");
+    scrollBtn.classList.remove("-show");
   }
 }
 
@@ -16,4 +19,19 @@ burgerBtn.addEventListener('click', () => {
 	document.querySelector('.header').classList.toggle("-active")
 	document.querySelector('.header__content-left-side').classList.toggle("-active")
 	burgerBtn.classList.toggle("-active")
+})
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+const video = document.querySelector(".video-thumbnail");
+
+video.addEventListener('click', () => {
+  video.innerHTML = `<iframe class="video-thumbnail__media" allowfullscreen="" allow="autoplay" src="https://www.youtube.com/embed/xpWZH6--aC4?rel=0&amp;showinfo=0&amp;autoplay=1"></iframe>`
 })
