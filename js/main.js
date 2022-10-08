df39a64b84cceda226d4d94f746e7e408fda1b08
Vladyslavzar <zarubinvlad12@gmail.com>
2022-10-09 00:47:52 +0300

    condition script load added

--- js/main.js
+++ js/main.js
@@ -35,3 +35,25 @@ const video = document.querySelector(".video-thumbnail");
 video.addEventListener('click', () => {
   video.innerHTML = `<iframe class="video-thumbnail__media" allowfullscreen="" allow="autoplay" src="https://www.youtube.com/embed/xpWZH6--aC4?rel=0&amp;showinfo=0&amp;autoplay=1"></iframe>`
 })
+
+const downloadJSAtOnload = (filename) => {
+  var element = document.createElement("script");
+  element.src = `${filename}.js`;
+  document.body.appendChild(element);
+}
+
+const aboutForms = document.querySelectorAll('.aboutContainerForm');
+let renderedChild = [];
+
+aboutForms.forEach((item, i) => {
+  item.querySelectorAll("input").forEach(elem => {
+    elem.addEventListener('focus', () => {
+      downloadJSAtOnload("diagram");
+      const hiddenElement = document.createElement("div");
+      hiddenElement.className = "diagram hidden"
+      if (renderedChild.includes(i)) return;
+      item.appendChild(hiddenElement);
+      renderedChild.push(i);
+    })
+  })
+})
