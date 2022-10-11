832f8916e006a579c9e6aa15b861472dd8906d69
Vladyslavzar <zarubinvlad12@gmail.com>
2022-10-11 21:00:44 +0300

    burger menu reworked main.js reworked

--- js/main.js
+++ js/main.js
@@ -1,59 +1,72 @@
+const init = () => {
+  const ifStartedScrolling = () => {
+    const scrollBtn = document.querySelector('.scroll-top-btn');
 
-window.addEventListener('scroll', headerResize);
-
-const scrollBtn = document.querySelector('.scroll-top-btn');
-
-function headerResize() {
-  if (document.documentElement.scrollTop !== 0) {
-    document.querySelector('.header').classList.add("-small");
-    scrollBtn.classList.add("-show");
-  } else {
-    document.querySelector('.header').classList.remove("-small");
-    scrollBtn.classList.remove("-show");
+    const headerResize = () => {
+      if (document.documentElement.scrollTop !== 0) {
+        document.querySelector('.header').classList.add("-small");
+        scrollBtn.classList.add("-show");
+      } else {
+        document.querySelector('.header').classList.remove("-small");
+        scrollBtn.classList.remove("-show");
+      }
+    }
+  
+    window.addEventListener('scroll', headerResize);
   }
-}
 
-const burgerBtn = document.querySelector('.headerContainerBurgerMenu');
+  ifStartedScrolling();
 
-burgerBtn.addEventListener('click', () => {
-	document.querySelector('.header').classList.toggle("-active")
-	document.querySelector('.header__content-left-side').classList.toggle("-active")
-	burgerBtn.classList.toggle("-active")
-})
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
+  const anchorScroll = () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
       });
-  });
-});
+    });
+  }
 
-const video = document.querySelector(".video-thumbnail");
+  anchorScroll();
 
-video.addEventListener('click', () => {
-  video.innerHTML = `<iframe class="video-thumbnail__media" allowfullscreen="" allow="autoplay" src="https://www.youtube.com/embed/xpWZH6--aC4?rel=0&amp;showinfo=0&amp;autoplay=1"></iframe>`
-})
+  const videoPlay = () => {
+    const video = document.querySelector(".video-thumbnail");
+  
+    video.addEventListener('click', () => {
+      video.innerHTML = `<iframe class="video-thumbnail__media" allowfullscreen="" allow="autoplay" src="https://www.youtube.com/embed/xpWZH6--aC4?rel=0&amp;showinfo=0&amp;autoplay=1"></iframe>`
+    })
+  }
+  
+  videoPlay();
 
-const downloadJSAtOnload = (filename) => {
-  var element = document.createElement("script");
-  element.src = `${filename}.js`;
-  document.body.appendChild(element);
+  const formsContentRender = () => {
+    const downloadJSAtOnload = (filename) => {
+      var element = document.createElement("script");
+      element.src = `js/${filename}.js`;
+      document.body.appendChild(element);
+    }
+    
+    const aboutForms = document.querySelectorAll('.aboutContainerForm');
+    let renderedChild = [];
+    
+    aboutForms.forEach((item, i) => {
+      item.querySelectorAll("input").forEach(elem => {
+        elem.addEventListener('focus', () => {
+          if (renderedChild.includes(i)) return;
+          downloadJSAtOnload(`diagram${i}`);
+          const hiddenElement = document.createElement("div");
+          hiddenElement.className = "diagram hidden"
+          item.appendChild(hiddenElement);
+          renderedChild.push(i);
+        })
+      })
+    })
+  }
+
+  formsContentRender();  
 }
 
-const aboutForms = document.querySelectorAll('.aboutContainerForm');
-let renderedChild = [];
+init();
 
-aboutForms.forEach((item, i) => {
-  item.querySelectorAll("input").forEach(elem => {
-    elem.addEventListener('focus', () => {
-      if (renderedChild.includes(i)) return;
-      downloadJSAtOnload(`diagram${i}`);
-      const hiddenElement = document.createElement("div");
-      hiddenElement.className = "diagram hidden"
-      item.appendChild(hiddenElement);
-      renderedChild.push(i);
-    })
-  })
-})
+
