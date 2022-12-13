8316fb92c48751c923080b4a4b01cc6cc4b79fb6
Vladyslavzar <zarubinvlad12@gmail.com>
2022-12-13 02:40:44 +0200

    load script fix

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
-const init = () => {
-  const anchorScroll = () => {
-    document.querySelectorAll('p[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        if(anchor.className.includes('js-save-diagram-in-pdf')) return;
-          e.preventDefault();
-          document.querySelector(this.getAttribute('href')).scrollIntoView({
-              behavior: 'smooth'
-          });
-      });
+const anchorScroll = () => {
+  document.querySelectorAll('p[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      if(anchor.className.includes('js-save-diagram-in-pdf')) return;
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
+  });
+}
+
+const downloadJSAtOnload = (filename) => {
+  var element = document.createElement("script");
+  element.src = filename;
+  element.type = 'module';
+  document.body.appendChild(element);
+}
+
+const formsContentRender = () => {
+  let isDone = false;
+  const baseUrl = window.location.pathname;
+  return () => {
+    if(!isDone) {
+      isDone = true;
+      downloadJSAtOnload(`${baseUrl}/js/loadCalculation.js`);
+    }
   }
 
+}
+
+const init = () => {
   anchorScroll();
 
-  const downloadJSAtOnload = (filename) => {
-    var element = document.createElement("script");
-    element.src = filename;
-    document.body.appendChild(element);
-  }
-
-  const formsContentRender = () => {
-    const baseUrl = window.location.pathname;
-
-    downloadJSAtOnload(`${baseUrl}/js/loadCalculation.js`)
-  }
-
-  formsContentRender();  
+  document.addEventListener('input', formsContentRender());  
 }
 
 init();
-
-
